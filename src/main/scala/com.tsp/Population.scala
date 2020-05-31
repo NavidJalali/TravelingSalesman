c4d12dd1bd11ec34ecee84bad7d6807489d0a0d3
furyofflames <nvdeth0@gmail.com>
2020-05-31 17:40:57 +0200

    fixy fix

--- src/main/scala/com.tsp/Population.scala
+++ src/main/scala/com.tsp/Population.scala
@@ -3,8 +3,8 @@ package com.tsp
 case class Population(value: Vector[Chromosome]) {
   def evolve: Population = ???
   @scala.annotation.tailrec
-  final def evolve(generations: Int)(soFar: Int = 1): Population =
-    if(soFar > generations) evolve.evolve(generations)(soFar + 1) else evolve
+  final def bulkEvolve(generations: Int)(soFar: Int = 1): Population =
+    if(soFar < generations) evolve.bulkEvolve(generations)(soFar + 1) else evolve
   def byFitness(costMatrix: CostMatrix, source: Int): Vector[(Chromosome, Int)] = value.map(chromosome =>
     (chromosome, Path((source +: chromosome.value) :+ source).cost(costMatrix).get)
   ).sortBy(_._2)

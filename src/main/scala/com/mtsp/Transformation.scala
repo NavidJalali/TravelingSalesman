885e229acaba733f64d8596e1e6201f627dbfe16
Navid <navid.jalali@prezi.com>
2020-09-30 21:11:19 +0200

    get rid of .idea

---
+++ src/main/scala/com/mtsp/Transformation.scala
@@ -0,0 +1,12 @@
+//package com.mtsp
+//
+//import com.tsp.{Constants, Cost, CostMatrix, Node}
+//
+//case class Transformation(costMatrix: CostMatrix, source: Node, adhocCities: Set[Node])
+//
+//object Transformation {
+//  def create(costMatrix: CostMatrix, source: Node, salesmen: Int): Transformation = {
+//    ???
+//  }
+//}
+
